51ea7a8e9c533d67e9079c3e538ec35028565dd7
Miskovy <mazenkhairy200@gmail.com>
2025-10-19 19:20:17 +0300

    Expand payment method CRUD feature tests
    Added new scenarios for creating, fetching, and updating payment methods, including success and failure cases, in the feature file. Implemented corresponding step definitions for validation of API responses, error handling, and status codes in the test class. Also updated project language level in misc.xml.

--- src/main/resources/features/F03_ADM_CRUDPaymentMethod.feature
+++ src/main/resources/features/F03_ADM_CRUDPaymentMethod.feature
@@ -14,9 +14,64 @@ Feature: an Admin has the ability to CRUD a Payment Method
   Scenario Outline: an Admin is able to create a Payment Method Successfully
     Given the API base URL
     When I send POST request to create Payment Method with "<name>" , "<discription>" , "<icon>" , "<type>"
-    Then the response status code should be 200
+    Then the response status code should be 200 due to success
     Then the response should contain success message of creation
-    Then the response should show the created payment method
+    Examples:
+      | name          | discription                    | icon                           | type   |
+      | Fawry | Fawry Test Description | iVBORw0KGgoAAAANSUhEUgAAAOE... | manual |
+
+  @FailTest
+  Scenario Outline: an Admin creates an existing payment method
+    Given the API base URL
+    When I send POST request to create Payment Method with "<name>" , "<discription>" , "<icon>" , "<type>"
+    Then the response status code should be 400 due to already exists
+    Then the response should contain fail message of creation due to already exists
     Examples:
       | name          | discription                    | icon                           | type   |
       | Vodafone Cash | Vodafone Cash Test Description | iVBORw0KGgoAAAANSUhEUgAAAOE... | manual |
+
+  @FailTest
+  Scenario Outline: an Admin POST a request with an empty field
+    Given the API base URL
+    When I send POST request to create Payment Method with "<name>" , "<discription>" , "<icon>" , "<type>"
+    Then the response status code should be 400 due to empty field
+    Then the response should contain fail message of not allowed
+    Examples:
+      | name          | discription                    | icon                           | type   |
+      |  | Vodafone Cash Test Description | iVBORw0KGgoAAAANSUhEUgAAAOE... | manual |
+
+  @SuccessTest
+  Scenario: an Admin is able to get all the payment Methods
+    Given  the API base URL
+    When I send a GET request to get all the payment methods
+    Then the response status code should be 200 when getting all payment fields
+    And all payment methods should be shown
+  @SuccessTest
+  Scenario Outline: an Admin is able to get a payment method by id
+    Given the API base URL
+    When I send a GET request with a parameter ID "<paymentMethodID>"
+    Then the response status code should be 200 when getting a payment method
+    And the requested payment is shown
+    Examples:
+      | paymentMethodID |
+      | 68f4fc7192992b812dd8db09 |
+  @FailTest
+  Scenario Outline: an Admin isn't able to get a payment method
+    Given the API base URL
+    When I send a GET request with a parameter ID "<paymentMethodID>"
+    Then the response status code should be 404 when getting a payment method
+    And the error message is shown that it's not found
+    Examples:
+      | paymentMethodID |
+      | 68f4fc7192992b812dd9db09 |
+
+
+  @SuccessTest
+  Scenario Outline: an Admin is able to update the requested payment method
+    Given the API base URL
+    When I send a PUT request with "<name>" in body , with id "<paymentMethodId>" to update payment method
+    Then the response status code should be 200 due to update successful
+    And the success message is shown for updating a payment method
+    Examples:
+      | name | paymentMethodId |
+      | Changed name Payment Test | 68f4fc7192992b812dd8db09 |
--- src/test/java/stepDefinitions/D03_ADM_CRUDPaymentMethod.java
+++ src/test/java/stepDefinitions/D03_ADM_CRUDPaymentMethod.java
@@ -1,9 +1,13 @@
 package stepDefinitions;
 
 import API.Variables;
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import org.apache.http.impl.conn.SystemDefaultRoutePlanner;
+import org.testng.Assert;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,5 +43,124 @@ public class D03_ADM_CRUDPaymentMethod {
         System.out.println("Status Code: "+ response.getStatusCode());
         System.out.println("Response Body: "+ response.body().asString());
 
+    }
+
+    @Then("the response should contain fail message of creation due to already exists")
+    public void theResponseShouldContainFailMessageOfCreation() {
+        response.then().body("error.message",notNullValue());
+        response.then().body("error.message",equalTo("Payment method already exists"));
+    }
+
+    @Then("the response status code should be {int} due to already exists")
+    public void theResponseStatusCodeShouldBeDueToAlreadyExists(int statusCode) {
+        response.then().body("error.code" , equalTo(statusCode));
+        System.out.println("The Received Status Code: "+ response.statusCode());
+    }
+
+    @Then("the response should contain success message of creation")
+    public void theResponseShouldContainSuccessMessageOfCreation() {
+        response.then().body("data.message",equalTo("Payment method created successfully"));
+        response.then().body("data.paymentMethod", notNullValue());
+    }
+
+    @Then("the response status code should be {int} due to success")
+    public void theResponseStatusCodeShouldBeDueToSuccess(int statusCode) {
+        response.then().statusCode(statusCode);
+        System.out.println("The Received Response Code is: "+ response.getStatusCode());
+    }
+
+    @Then("the response status code should be {int} due to empty field")
+    public void theResponseStatusCodeShouldBeDueToEmptyField(int statusCode) {
+        response.then().statusCode(statusCode);
+        System.out.println("The Received Response Code is: "+ response.getStatusCode());
+    }
+
+    @Then("the response should contain fail message of not allowed")
+    public void theResponseShouldContainFailMessageOfIsRequired() {
+        response.then().body("error.message", notNullValue());
+        response.then().body("error.message", containsString("is not allowed to be empty"));
+        response.then().body("error.details[0]", containsString("is not allowed to be empty"));
+    }
+
+    @When("I send a GET request to get all the payment methods")
+    public void iSendAGETRequestToGetAllThePaymentMethods() {
+        String authToken = AuthTokenHolder.getToken();
+        response = given()
+                .header("Content-Type" ,"application/json")
+                .header("Authorization", "Bearer " + authToken)
+                .when()
+                .get(apiVariables.getPAYEMNT_METHOD_END())
+                .then().log().all()
+                .extract().response();
+        System.out.println("\n=== GET ALL PAYMENT METHOD ===");
+        System.out.println("Status Code: "+ response.getStatusCode());
+        System.out.println("Response Body: "+ response.body().asString());
+    }
+
+    @Then("the response status code should be {int} when getting all payment fields")
+    public void theResponseStatusCodeShouldBeWhenGettingAllPaymentFields(int statusCode) {
+        response.then().statusCode(statusCode);
+        System.out.println("The Received Response Code is: "+ response.getStatusCode());
+    }
+
+    @And("all payment methods should be shown")
+    public void allPaymentMethodsShouldBeShown() {
+        response.then().body("data.message", equalTo("All payment methods fetched successfully"));
+        response.then().body("data.paymentMethods",notNullValue());
+    }
+
+    @When("I send a GET request with a parameter ID {string}")
+    public void iSendAGETRequestWithAParameterID(String paymentMethodId) {
+        String authToken = AuthTokenHolder.getToken();
+        response = given()
+                .header("Content-Type" ,"application/json")
+                .header("Authorization", "Bearer " + authToken)
+                .when()
+                .get(apiVariables.getPAYEMNT_METHOD_END() +"/"+ paymentMethodId)
+                .then().log().all().extract().response();
+        System.out.println("\n=== GET A PAYMENT METHOD ===");
+        System.out.println("Status Code: "+ response.getStatusCode());
+        System.out.println("Response Body: "+ response.body().asString());
+    }
+
+    @Then("the response status code should be {int} when getting a payment method")
+    public void theResponseStatusCodeShouldBeWhenGettingAPaymentMethod(int statusCode) {
+        response.then().statusCode(statusCode);
+    }
+
+    @And("the requested payment is shown")
+    public void theRequestedPaymentIsShown() {
+        response.then().body("data.message" , equalTo("Payment method fetched successfully"));
+        response.then().body("data.paymentMethod", notNullValue());
+        response.then().body("data.paymentMethod._id", notNullValue());
+        response.then().body("data.paymentMethod.name" , notNullValue());
+    }
+
+    @And("the error message is shown that it's not found")
+    public void theErrorMessageIsShownThatItSNotFound() {
+        response.then().body("error.message", notNullValue());
+        response.then().body("error.message", equalTo("Payment method not found"));
+    }
+
+    @When("I send a PUT request with {string} in body , with id {string} to update payment method")
+    public void iSendAPUTRequestWithInBodyWithIdToUpdatePaymentMethod(String arg0, String id) {
+        String authToken = AuthTokenHolder.getToken();
+        Map<String , Object> layoutBody = new HashMap<>();
+        layoutBody.put("name",arg0);
+        response = given()
+                .header("Content-Type" ,"application/json")
+                .header("Authorization", "Bearer " + authToken)
+                .body(layoutBody)
+                .when()
+                .put(apiVariables.getPAYEMNT_METHOD_END()+"/"+id)
+                .then().log().all().extract().response();
+        System.out.println("\n=== UPDATE A PAYMENT METHOD ===");
+        System.out.println("Status Code: "+ response.getStatusCode());
+        System.out.println("Response Body: "+ response.body().asString());
+    }
+
+    @Then("the response status code should be {int} due to update successful")
+    public void theResponseStatusCodeShouldBeDueToUpdateSuccessful(int statusCode) {
+        response.then().statusCode(statusCode);
     }
 }
